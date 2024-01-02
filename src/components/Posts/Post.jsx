5b7605085cd90fa1c445ebdcbccdadc795246138
jitheshm <jitheshmjithooz@gmail.com>
2024-01-02 17:25:09 +0530

    products are displayed in home page

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import Heart from '../../assets/Heart';
 import './Post.css';
-
+import { firebaseContext } from '../../store/Context';
+import { collection, doc, getDoc, getDocs, getFirestore } from 'firebase/firestore';
 function Post() {
+  const [products, setProducts] = useState([])
+  const { firebase } = useContext(firebaseContext)
+  useEffect(() => {
+    try {
+      (async function () {
+        const db = getFirestore(firebase);
+        const querySnapshot = await getDocs(collection(db, "products"));
+        const allpost=[]
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          //console.log(doc.id, " => ", doc.data());
+          allpost.push({
+            ...doc.data(),
+            id:doc.id
+          })
+        });
+        setProducts(allpost)
+      })()
+    } catch (error) {
+      console.log(error);
+    }
+
+
+
+  }, [])
+ 
+
 
   return (
     <div className="postParentDiv">
-      
+
       <div className="recommendations">
         <div className="heading">
           <span>Fresh recommendations</span>
         </div>
         <div className="cards">
-          <div className="card">
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
-            </div>
-            <div className="date">
-              <span>10/5/2021</span>
-            </div>
-          </div>
+          {
+            products.map((product) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <div className="card">
+                  <div className="favorite">
+                    <Heart></Heart>
+                  </div>
+                  <div className="image">
+                    <img src={product.img} alt="" />
+                  </div>
+                  <div className="content">
+                    <p className="rate">&#x20B9; {product.price}</p>
+                    <span className="kilometer">{product.category}</span>
+                    <p className="name"> {product.name}</p>
+                  </div>
+                  <div className="date">
+                    <span>{product.createdAt}</span>
+                  </div>
+                </div>
+              )
+
+            })
+          }
+
         </div>
       </div>
     </div>
